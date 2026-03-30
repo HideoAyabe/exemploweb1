28c13a7fee0fd2b5141a1d11690643c2b35d8a2a
hyan <hyan.ha3@gmail.com>
2026-03-30 09:15:31 -0300

    feat:declarações e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,46 @@
-alert("hello world!!!!!!!!!!!!!!!!!!!!!!!!")
+//declarações e variaveis
+//var, let e const
+
+var nome="fiap";
+console.log(nome);
+
+var nome="teste";
+console.log(nome);
+
+if(true){
+    var apelido="fiapinho";
+    console.log(apelido)
+}
+
+const taxajuros=0.05;
+console.log(taxajuros)
+
+//typeof verifica o tipo da variavel
+var exemplo="ola-dev-var";
+console.log(exemplo);
+let exemplo1="ola-devlet";
+console.log(exemplo1);
+const exemplo2="ola-dev-const";
+console.log(exemplo2)
+
+let exemplo3=10;
+console.log(exemplo3)
+
+let exemplo4=true;
+console.log(exemplo4);
+
+let exemplo5={nome1:"teste"};
+console.log(exemplo5);
+
+let exemplo6=["java","c#","php"];
+console.log(exemplo6);
+
+//valor undefined
+
+let exemplo7;
+console.log(exemplo7)
+
+//valor vazio
+let exemplo8 =null;
+console.log(exemplo8);
+
