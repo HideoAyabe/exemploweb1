cc713ef1724fa7005e99e723ea39091c8362a1ff
hyan <hyan.ha3@gmail.com>
2026-04-06 09:10:42 -0300

    feat: estruturas condicionais

--- src/js/script.js
+++ src/js/script.js
@@ -1,86 +1,164 @@
-//declarações e variaveis
-//var, let e const
+// //declarações e variaveis
+// //var, let e const
 
-var nome="fiap";
-console.log(nome);
+// var nome="fiap";
+// console.log(nome);
 
-var nome="teste";
-console.log(nome);
+// var nome="teste";
+// console.log(nome);
+
+// if(true){
+//     var apelido="fiapinho";
+//     console.log(apelido)
+// }
+
+// const taxajuros=0.05;
+// console.log(taxajuros)
+
+// //typeof verifica o tipo da variavel
+// var exemplo="ola-dev-var";
+// console.log(exemplo);
+// let exemplo1="ola-devlet";
+// console.log(exemplo1);
+// const exemplo2="ola-dev-const";
+// console.log(exemplo2)
+
+// let exemplo3=10;
+// console.log(exemplo3)
+
+// let exemplo4=true;
+// console.log(exemplo4);
+
+// let exemplo5={nome1:"teste"};
+// console.log(exemplo5);
+
+// let exemplo6=["java","c#","php"];
+// console.log(exemplo6);
+
+// //valor undefined
+
+// let exemplo7;
+// console.log(exemplo7)
+
+// //valor vazio
+// let exemplo8 =null;
+// console.log(exemplo8);
+
+// //operadores aritmeticos
+
+// const valor1=10;
+// const valor2=20;
+// console.log(valor1 + valor2);
+// console.log(valor1 - valor2);
+// console.log(valor1 * valor2);
+// console.log(valor1 / valor2);
+
+// //operadores logicos
+// // = atribuição
+// // == comparação
+// // === valor e tipo(estritamente igual)
+// // && = e (todas condições tem de ser verdadeiras)
+// // || = ou (uma das condição tem que ser verdadeira)
+// console.log(valor1 < valor2);
+// console.log(valor1 > valor2 && valor2 < 10);
+// console.log(valor2 >100 || valor1 <10);
+// console.log(valor1 == valor2);
+// console.log(valor2 === valor1)
+
+// //exercicios
+
+// let p=10;
+// let v=3;
+// let precofinal = p - v;
+// console.log(precofinal);
+
+// //exercicio2
+
+// let a=20
+// let b=10
+// console.log(a === 20 && b >= 10);
+
+
+// //exercicio3
+
+// const usuario = true;
+// const token = "bahia"
+
+// const transacao =usuariologado ===2 
+// console.log("transação autorizada",transacao)
+
+
+// //estrutura condicional
+
+//if
 
 if(true){
-    var apelido="fiapinho";
-    console.log(apelido)
+    console.log("verdadeiro")
+};
+
+let nome="fiap";
+
+if(nome =="fiap"){
+    console.log("nome correto")
 }
 
-const taxajuros=0.05;
-console.log(taxajuros)
+// if/else
 
-//typeof verifica o tipo da variavel
-var exemplo="ola-dev-var";
-console.log(exemplo);
-let exemplo1="ola-devlet";
-console.log(exemplo1);
-const exemplo2="ola-dev-const";
-console.log(exemplo2)
+if(nome =="Fiap"){
+    console.log("usuario correto")
+}else{
+    console.log("usuario errado")
+}
 
-let exemplo3=10;
-console.log(exemplo3)
+// if encadeado/aninhado
 
-let exemplo4=true;
-console.log(exemplo4);
+let idade = 47;
 
-let exemplo5={nome1:"teste"};
-console.log(exemplo5);
+if(idade <= 18){
+    console.log("é uma criança")
+}else if(idade >13 && idade <=18){
+    console.log("é um adolescente")
+}else if(idade >18 && idade <= 50){
+    console.log("é um adulto")
+}else{
+    console.log("é um idoso")
+}
 
-let exemplo6=["java","c#","php"];
-console.log(exemplo6);
+// switch case
+/*
+switch = escolhas
+case = resultado da escolha
+break = parar a ação
+default = resultado base 
+*/
 
-//valor undefined
+let pratos="salada";
 
-let exemplo7;
-console.log(exemplo7)
+switch(pratos){
+    case "macarrão":
+        console.log("a melhor massa")
+        break;
+    case "salada":
+        console.log("salada saudavel")
+        break;
+    case "lasanha":
+        console.log("melhor prato")
+        break;
+    default:
+        console.log("nenhuma das opções")
+}
 
-//valor vazio
-let exemplo8 =null;
-console.log(exemplo8);
+//ternario 
+/*
+? = if
+: = else
+*/
+let valor = 100;
+let resultado = valor ==100 ? "valor certo": "valor errado";
+console.log(resultado)
 
-//operadores aritmeticos
+let usuario="dev";
+let logado= usuario =="fiap" ? "usuario logado" : "usuario invalido";
+console.log(logado)
 
-const valor1=10;
-const valor2=20;
-console.log(valor1 + valor2);
-console.log(valor1 - valor2);
-console.log(valor1 * valor2);
-console.log(valor1 / valor2);
-
-//operadores logicos
-// = atribuição
-// == comparação
-// === valor e tipo(estritamente igual)
-// && = e (todas condições tem de ser verdadeiras)
-// || = ou (uma das condição tem que ser verdadeira)
-console.log(valor1 < valor2);
-console.log(valor1 > valor2 && valor2 < 10);
-console.log(valor2 >100 || valor1 <10);
-console.log(valor1 == valor2);
-console.log(valor2 === valor1)
-
-//exercicios
-
-let p=10;
-let v=3;
-let precofinal = p - v;
-console.log(precofinal);
-
-//exercicio2
-
-let a=20
-let b=10
-console.log(a === 20 && b >= 10);
-
-
-//exercicio3
-
-let usuario = true;
-let token = "bahia"
-console.log(usuario==true & token!=null)
+//
